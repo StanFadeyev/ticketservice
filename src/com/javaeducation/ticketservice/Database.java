ab7b9e186ce1e1005729bf408be47552146ae3c5
Stanislav Fadeev <incvista@gmail.com>
2019-05-25 15:02:20 +0300

    - method Add for class Database - comment some string with id param

--- src/com/javaeducation/ticketservice/Database.java
+++ src/com/javaeducation/ticketservice/Database.java
@@ -5,6 +5,28 @@ package com.javaeducation.ticketservice;
 import java.util.ArrayList;
 
 public class Database {
-    ArrayList <Storage> database = new ArrayList<>();
+    ArrayList<Storage> database = new ArrayList<>();
+    int id; //(database.size() == 0 ? 1 : (database.size() - 1));
 
+    // посмотреть видео, где выносится уникальный id - udemy;
+    public String add(String[] args) {
+
+        if (database.size() == 0) {
+            id = 0;
+        } else {
+            id = (database.size() - 1);
+        }
+        String[] k = {" ", " ", " ", " "};
+
+        for (int i = 0; i < args.length && i < k.length; i++)
+        {
+            k[i] = args[i];
+        }
+        Storage s = new Storage(k[0], k[1], k[2], k[3]);
+        database.add(s);
+        return s.toString();
+    }
+   /* public String delete (){
+
+    }*/
 }
