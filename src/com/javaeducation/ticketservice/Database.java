40746ba3ec8d24478c644363cf62596dd98189ba
Stanislav Fadeev <incvista@gmail.com>
2019-05-27 23:35:24 +0300

    add dubious condition fot Exit and Default in main class. Maybe need rework Command enum

--- src/com/javaeducation/ticketservice/Database.java
+++ src/com/javaeducation/ticketservice/Database.java
@@ -6,27 +6,27 @@ import java.util.ArrayList;
 
 public class Database {
     ArrayList<Storage> database = new ArrayList<>();
-    int id; //(database.size() == 0 ? 1 : (database.size() - 1));
+    int id = 0;
 
     // посмотреть видео, где выносится уникальный id - udemy;
     public String add(String[] args) {
-
-        if (database.size() == 0) {
+        int id = (database.size() == 0 ? 1 : (database.size() - 1));
+        /*if (database.size() == 0) {
             id = 0;
         } else {
             id = (database.size() - 1);
-        }
+        }*/
         String[] k = {" ", " ", " ", " "};
 
-        for (int i = 0; i < args.length && i < k.length; i++)
-        {
+        for (int i = 0; i < args.length && i < k.length; i++) {
             k[i] = args[i];
         }
         Storage s = new Storage(k[0], k[1], k[2], k[3]);
         database.add(s);
         return s.toString();
     }
-   /* public String delete (){
+/*    public String delete (int id){
+        int index = -1;
 
     }*/
 }
