70901a9b9db7b4b3e260abf5229a6c7a96fe31e6
Stanislav Fadeev <incvista@gmail.com>
2019-05-27 23:34:37 +0300

    add dubious condition fot Exit and Default in main class. Maybe need rework Command enum

--- src/com/javaeducation/ticketservice/Ticketservice.java
+++ src/com/javaeducation/ticketservice/Ticketservice.java
@@ -10,32 +10,46 @@ public class Ticketservice {
         System.out.println("Enter command: ");
         String data = ticketData.nextLine();
 
-        switch (Command.getCommand(data)) {
-            case ADD: {
-                String[] substr = data.substring(3).trim().split(",");
-//                System.out.println("this is add");
-                break;
-            }
-            case UPDATE:
-                System.out.println("this is update");
-                break;
-            case DELETE:
-                System.out.println("this is delete");
-                break;
-            case SHOW:
-                System.out.println("this is show");
-                break;
-            case SHOWALL:
-                System.out.println("this is showAll");
-                break;
-            case EXIT:
-                System.out.println("this is exit");
-                break;
-            case DEFAULT:
-                System.out.println("this is peace of sh#t");
-                break;
+        while (!data.equalsIgnoreCase("exit")) {
 
-        }
+            switch (Command.getCommand(data)) {
+                case ADD: {
+                    String[] substr = data.substring(3).trim().split(",");
+                    Database example = new Database();
+                    System.out.println(example.id + " " + example.add(substr));
+                    System.out.println("Enter command: ");
+                    data = ticketData.nextLine();
+//                System.out.println("this is add");
+                    break;
+                }
+                case UPDATE:
+                    System.out.println("this is update");
+                    break;
+                case DELETE:
+                    System.out.println("this is delete");
+                    break;
+                case SHOW:
+                    System.out.println("this is show");
+                    break;
+                case SHOWALL:
+                    System.out.println("this is showAll");
+                    break;
+ /*               case EXIT:
+                    System.out.println("this is exit");
+                    break;
+                case DEFAULT:
+                    System.out.println("this is peace of sh#t");
+                    break;*/
+
+            }
+           // wrong code hereinafter - need rework Command enum
+            if (data.equalsIgnoreCase("exit")) {
+                System.out.println((Command.getCommand(data).arguments));
+
+            } else
+                System.out.println((Command.getCommand(data).arguments));
+
+            }
         ticketData.close();
     }
 }
