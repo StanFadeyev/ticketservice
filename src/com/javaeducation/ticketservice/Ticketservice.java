249dfeeac25d9c4d8c7d5783061af5d5fcdfe990
Stanislav Fadeev <incvista@gmail.com>
2019-06-13 03:18:32 +0300

    1. add reading command from string through .next() 2. add reading arguments from string through .nextLine() in Add case

--- src/com/javaeducation/ticketservice/Ticketservice.java
+++ src/com/javaeducation/ticketservice/Ticketservice.java
@@ -13,10 +13,11 @@ public class Ticketservice {
         readLoop:
         do {
             System.out.println("Enter command: ");
-            String data = ticketData.nextLine().trim();
-            switch (Command.getCommand(data)) {
+            String dataCommand = ticketData.next().trim();
+            switch (Command.getCommand(dataCommand)) {
                 case ADD: {
-                    String[] input = data.substring(3).trim().split(",");
+                    String data = ticketData.nextLine().trim();
+                    String[] input = data.trim().split(",");
                     if (input.length == 4) //throw new IOException("Need 4 params only!\n");
                     {
                         for (int i = 0; i < input.length; i++) {
@@ -38,7 +39,7 @@ public class Ticketservice {
                     }
                     break;
                 }
-                case UPDATE: {
+/*                case UPDATE: {
                     String[] substr = data.substring(6).trim().split(",");
                     if (substr.length == 5) //throw new IOException("Need 5 params only!");
                     {
@@ -125,10 +126,10 @@ public class Ticketservice {
                 case DEFAULT: {
                     System.out.println(Command.DEFAULT.arguments);
                     break;
-                }
-            }
+        */        }
+ //           }
 
         } while (true);
-        ticketData.close();
+ //       ticketData.close();
     }
 }
